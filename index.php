4d2f1b8bc261419c9f62bde3daa0f0d0ea67263b
MiniNaim <mininaim@gmail.com>
2022-09-25 11:16:38 +0100

    Refactore codebase

--- index.php
+++ index.php
@@ -1,144 +1,81 @@
-<!doctype html>
-<html lang="ar" dir="rtl">
+<?php
 
-<head>
+$page = $_GET['page'];
 
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+require_once 'templates/commons/header.html';
+switch ($cmd) {
 
-    <!-- Extra Meta-->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name="generator" content="">
+        // default/home
+    default:
 
-    <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
-    <link rel="manifest" href="assets/favicon/site.webmanifest">
-    <link rel="mask-icon" href="assets/favicon/safari-pinned-tab.svg" color="#8662a3">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="theme-color" content="#ffffff">
-    <!-- <link rel="icon" href="assets/favicon/favicon.ico"> -->
+        // componments 
+        include_once 'templates/componments/alert.html';
+        include_once 'templates/componments/top-navigation.html';
 
-    <!-- Open Graph -->
-    <meta property="og:title" content="">
-    <meta property="og:description" content="">
-    <meta property="og:image" content="">
-    <meta property="og:url" content="">
-    <meta property="og:site_name" content="">
-    <meta property="og:type" content="website">
-    <meta property="og:locale" content="ar_AR">
-    <meta property="og:locale:alternate" content="en_US">
+        include_once 'templates/componments/cart-offcanvas.html';
+        include_once 'templates/componments/menu-offcanvas.html';
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.rtl.min.css"
-        integrity="sha384-OXTEbYDqaX2ZY/BOaZV/yFGChYHtrXH2nyXJ372n2Y8abBhrqacCEe+3qhSHtLjy" crossorigin="anonymous">
+        include_once 'templates/componments/main-navigation.html';
 
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@400;600&display=swap" rel="stylesheet">
+        // home
+        include_once 'templates/home/slider.html';
+        include_once 'templates/home/intro.html';
+        include_once 'templates/home/categories.html';
+        include_once 'templates/home/featured.html';
+        include_once 'templates/home/discount.html';
+        include_once 'templates/home/best-seller.html';
+        include_once 'templates/home/recently-arrived.html';
+        include_once 'templates/home/partners.html';
+        include_once 'templates/home/reviews.html';
 
-    <!-- Slick -->
-    <!-- <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" /> -->
+        #include_once 'templates/swiper.html';
+        #include_once 'templates/snap-scrolling-test.html';
+        break;
 
-    <!-- Swiper -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+        // 404
+    case '404':
+        include_once 'templates/pages/404.html';
+        break;
 
-    <!-- Iconoir & Feather -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css">
-    <script src="addons/feather-icons/dist/feather.min.js"></script>
+        // search no results
+    case 'search-no-results':
+        include_once 'templates/componments/hero-global.html';
+        include_once 'templates/pages/search-no-results.html';
+        break;
 
-    <!-- CSS Files -->
-    <link rel="stylesheet" href="assets/css/fonts.css">
-    <link rel="stylesheet" href="assets/css/theme.css">
-    <link rel="stylesheet" href="assets/css/global.css">
+        // products
+    case 'products':
+        include_once 'templates/products/hero.html';
+        include_once 'templates/products/list.html';
+        include_once 'templates/products/filters-offcanvas.html';
+        break;
 
-    <!-- Components -->
-    <link rel="stylesheet" href="assets/css/components/triangle.css">
-    <link rel="stylesheet" href="assets/css/components/effects.css">
-    <link rel="stylesheet" href="assets/css/components/snap.css">
-    <link rel="stylesheet" href="assets/css/components/links.css">
-    <link rel="stylesheet" href="assets/css/components/decorators.css">
-    <link rel="stylesheet" href="assets/css/components/swiper.css">
-    <link rel="stylesheet" href="assets/css/components/css-filter.css">
+        // categories
+    case 'categories':
+        include_once 'templates/categories/hero.html';
+        include_once 'templates/categories/list.html';
+        break;
 
-    <!-- Title -->
-    <title>زد - القالب الإفتراضي</title>
+        // category
+    case 'category':
+        include_once 'templates/categories/hero-single.html';
+        include_once 'templates/categories/list-single.html';
+        break;
 
-</head>
+        // cart empty
+    case 'cart-empty':
+        include_once 'templates/componments/hero-global.html';
+        include_once 'templates/pages/cart-empty.html';
+        break;
 
-<body>
+        // cart view
+    case 'cart-view':
+        include_once 'templates/pages/cart-view.html';
+        break;
+}
 
-    <!-- Tomporary alert for Mobile Devices -->
-    <!-- <section class="d-block d-lg-none">
-        <div class="alert bg-gradient d-flex flex-column text-center" role="alert">
-            <i class="iconoir-hand-brake iconoir-lg"></i>
+require_once 'templates/commons/footer-inner.html';
+require_once 'templates/commons/footer.html';
 
-            <p class="lead mt-3">
-                فضلا التصفح من شاشة جهاز كمبيوتر، أو متصفح بأبعاد كبيرة، الموبايل قادم في القريب العاجل
-            </p>
-        </div>
-    </section> -->
 
-    <!-- Tomporary wrapper for Desktop & large Screens -->
-    <!-- <section class="d-none d-lg-block"> -->
-
-    <!-- Progress Loader -->
-    <div id="progress"></div>
-
-    <!-- Components -->
-    <?php //include_once 'templates/swiper.html'; 
-    ?>
-
-    <?php include_once 'templates/alert.html'; ?>
-    <?php include_once 'templates/top-navigation.html'; ?>
-    <?php include_once 'templates/main-navigation.html'; ?>
-    <?php include_once 'templates/slider.html'; ?>
-    <?php include_once 'templates/intro.html'; ?>
-    <?php include_once 'templates/categories.html'; ?>
-    <?php include_once 'templates/featured.html'; ?>
-    <?php include_once 'templates/discount.html'; ?>
-    <?php include_once 'templates/best-seller.html'; ?>
-    <?php include_once 'templates/recently-arrived.html'; ?>
-    <?php include_once 'templates/partners.html'; ?>
-    <?php include_once 'templates/reviews.html'; ?>
-
-    <?php
-    //include_once 'templates/snap-scrolling-test.html'; 
-    ?>
-
-    <?php include_once 'templates/footer.html'; ?>
-    <!-- End of Components -->
-
-    <!-- </section> -->
-    <!-- Tomporary wrapper for Desktop & large Screens -->
-
-    <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
-    </script>
-
-    <!-- jQuery Dependecies -->
-    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-
-    <!-- Swiper -->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-
-    <!-- JS Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
-        integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous"
-        async></script>
-    <!-- <script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script> -->
-    <script src="addons/progressbar.js/dist/progressbar.min.js"></script>
-
-    <!-- JS Application -->
-    <script src="assets/js/app.js"></script>
-
-</body>
-
-</html>
+include_once 'templates/componments/hero-global.html';
--- readme.md
+++ readme.md
@@ -1,24 +1,53 @@
-## Default Theme for Zid catalog
+## Zid Catalog - Default Theme
+
+### Redesign Pages
+### Done :check: 
+- ~~[Home Page] (https://zid-demo.com/)~~
+- ~~[Categories] (https://zid-demo.com/categories)~~
+- ~~[Products] (https://zid-demo.com/products)~~
+- ~~[Category] (https://zid-demo.com/categories/388905)~~
+- ~~[Cart View] (https://zid-demo.com/cart/view)~~
+
+### Upcoming :ongoing:
+- [Terms and Conditions] (https://zid-demo.com/pages/terms-and-conditions)
+- [Shipping and Payment] (https://zid-demo.com/pages/shipping-and-payment)
+- [Product Sample 1] (https://zid-demo.com/products/)%D9%85%D8%B9%D8%B7%D9%81-%D8%A8%D9%86%D9%8A-%D9%81%D8%A7%D8%AA%D8%AD-%D8%A8%D8%B3%D8%AD%D8%A7%D8%A8-%D8%B1%D8%AC%D8%A7%D9%84%D9%8A)
+- [Product Sample 2] (https://zid-demo.com/products/%D9%85%D8%B9%D8%B7%D9%81-%D9%83%D8%A7%D8%B4%D9%8A%D9%87-%D9%83%D8%AD%D9%84%D9%8A-%D8%A8%D8%B7%D8%A8%D8%B9%D8%A9-%D8%B1%D8%AC%D8%A7%D9%84%D9%8A)
+- [Product Sample 3] (https://zid-demo.com/products/%D9%85%D8%B9%D8%B7%D9%81-%D9%85%D8%B7%D8%B1-%D8%B1%D9%85%D8%A7%D8%AF%D9%8A-%D8%A8%D8%AC%D9%8A%D9%88%D8%A8-%D8%B1%D8%AC%D8%A7%D9%84%D9%8A)
+- [Product Sample 4] (https://zid-demo.com/products/%D9%85%D8%B9%D8%B7%D9%81-%D8%A3%D8%B3%D9%88%D8%AF-%D8%A8%D9%8A%D8%A7%D9%82%D8%A9-%D9%81%D8%B1%D9%88-%D8%B1%D8%AC%D8%A7%D9%84%D9%8A)
+
+### Minor Fixes
+- [ ] Fix height of images
+- [ ] Change arrows in categories
+- [ ] Fix arrows in all sliders
+- [ ] Find solutoion for search results bar in homepage
 
 ### Todo
 - [ ] Clean codebase
-- [ ] Generate color palette
-- [ ] Implement a better way to handle the sliders
-- [ ] Responsiveness
+- [ ] Generate color palette for the theme
+- [ ] Responsiveness for all pages
 - [ ] Add a link to the demo
 
-### Upcoming
-- replace slick to get rid of jquery dependecie (or use jquery slim)
-- generate color pallete
-- add animation
-- change photos of products, categories, slider
-- replace node-modules with unpkg/jsdelivr resources
-- integrate vite
-- replcae /addons/ with unpkg/jsdelivr resources 
+### Requests
+- [ ] Ask about all use-cases and hidden senarios that should be supported
+- [ ] Change photos of products, categories, slider to new/better ones
+
+### Labs
+- [ ] Delete node modules and unpkg folders
+- [ ] Integrate Vite
 
 ### Credits
 - [Bootstrap](https://getbootstrap.com/)
-- [Slick](https://kenwheeler.github.io/slick/)
-- [Font Awesome](https://fontawesome.com/)
 - [jQuery](https://jquery.com/)
 - [Popper.js](https://popper.js.org/)
+- [Swiper](https://swiperjs.com/)
+
+### Libraries to consider/test
+- [Font Awesome](https://fontawesome.com/)
+- [Owl Carousel](https://owlcarousel2.github.io/OwlCarousel2/)
+- [Animate.css](https://daneden.github.io/animate.css/)
+- [jQuery Easing](http://gsgd.co.uk/sandbox/jquery/easing/)
+- [Sticky Kit](http://leafo.net/sticky-kit/)
+- [Isotope](https://isotope.metafizzy.co/)
+- [ImagesLoaded](https://imagesloaded.desandro.com/)
+- [jQuery Countdown](https://hilios.github.io/jQuery.countdown/)
